4cb8fbbf4f72fff0ef4b7843ff51b75c9b8977e4
Anthony Tran <113148499+Tranladdy@users.noreply.github.com>
2023-11-27 13:31:58 -0800

    Adding Source Files
    Source files containing the javascript code for the wesbite and also the css file.

---
+++ src/data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+    {
+        id: "asdhaodi",
+        name: "Mime Event",
+        price: 5.00,
+        desc: "A show for a performance of mimes in their daily lives.",
+        img: "https://ischoolconnect.com/blog/wp-content/uploads/2022/09/drama.jpg"
+    },
+    {
+        id: "asdoiashdoid",
+        name: "Into the Woods",
+        price: 17.00,
+        desc: "A play where a wife and husband go on a journey into the woods to break a curse.",
+        img: "https://assets.playbill.com/editorial/_articleLeadImage/12bf5706e0684ef7eb28c884da664d6a-sondheimbday-itw-hr.jpg"
+    },
+    {
+        id: "dasndasd",
+        name: "Frankenstein",
+        price: 20.00,
+        desc: "A story of gifted scientist Victor Frankenstein who succeeds in giving life to a being of his own creation.",
+        img: "https://static01.nyt.com/images/2007/11/09/theater/Young1650.jpg?quality=75&auto=webp&disable=upscale"
+    },
+    {
+        id: "daodashodi",
+        name: "Winnie the Pooh",
+        price: 10.00,
+        desc: "A story of the classic Disney character Winnie the Pooh.",
+        img: "https://i0.wp.com/kidfriendlydc.com/wp-content/uploads/2013/02/pooh_1-600x480.jpg?resize=500%2C400"
+    },
+    {
+        id: "bciasbvuifd",
+        name: "Shrek",
+        price: 25.00,
+        desc: "A play on a story of the classic Dreamworks character Shrek.",
+        img: "https://parentpreviews.com/images/made/legacy-pics/shrek-the-musical_668_330_80_int_s_c1.jpg"
+    },
+    {
+        id: "anodaofib",
+        name: "The Giver",
+        price: 13.00,
+        desc: "A play on a story of the classic novel 'The Giver', a story on a dystopian world.",
+        img: "https://www.dramaticpublishing.com/media/gallery/galleryimages//g/9/g91irt1.jpg"
+    },
+    {
+        id: "souigdfrgnbl",
+        name: "Macbeth",
+        price: 16.00,
+        desc: "A play that is set in medieval Scotland and is partly based on a true historical account.",
+        img: "https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/LBJCTYVQQYI6RLWZAAJQTGIHO4.jpg"
+    },
+    {
+        id: "suefwebgf",
+        name: "Mime Event",
+        price: 13.00,
+        desc: "A play on Prince Hamlet of Denmark who seeks revenge against his uncle Claudius, the murderer of his father.",
+        img: "https://www.theoldglobe.org/globalassets/images/2016-2017/hamlet/production-photos/hamlet-26-web.jpg?id=27296"
+    }
+];
---
+++ src/main.js
@@ -0,0 +1,85 @@
+let shop = document.getElementById("shop");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let generateShop =() => {
+    return (shop.innerHTML = shopItemsData
+        .map((x) => {
+            let {id, name, price, desc, img} =x;
+            let search = basket.find((x)=>x.id === id) || [];
+            return  `
+        <div id=prodict-id-${id} class="detail-card">
+            <img class="detail-img" src= ${img}>
+                <div class="detail-desc">
+                    <div class="detail-name">
+                        <h4>${name}</h4>
+                        <p class="detail-sub">${desc}</p>
+                            <div class="price-quantity">
+                                    <p class="price">$ ${price}.00</p>
+                                    <div class="buttons">
+                                        <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                                        <div id=${id} class="quantity">
+                                        ${search.item === undefined ? 0 : search.item}
+                                        </div>
+                                        <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        `;
+    })
+    .join(""));
+    
+};
+generateShop();
+
+let increment = (id)=>{
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id);
+
+    if(search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    }
+    else{
+        search.item += 1;
+    }
+
+    update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
+    //console.log(basket);
+}
+
+let decrement = (id)=>{
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id)
+
+    if(search === undefined) return;
+    else if(search.item === 0) return;
+    else{
+        search.item -= 1;
+    }
+
+    update(selectedItem.id);
+    basket = basket.filter((x)=>x.item !== 0);
+    //console.log(basket);
+    localStorage.setItem("data", JSON.stringify(basket));
+}
+
+let update = (id)=>{
+    let search = basket.find((x)=>x.id === id);
+    document.getElementById(id).innerHTML = search.item;
+    calculation()
+};
+
+let calculation = ()=>{
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x)=>x.item).reduce((x,y)=>x+y,0);
+}
+
+calculation();
+
+
